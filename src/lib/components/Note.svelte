5f5ca8397a406272d0daf9b72519bed137776dc4
lgmdor <lgm.dor@gmail.com>
2023-01-05 01:00:43 +0100

    removed note title bar

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -4,11 +4,11 @@
 	import Dropdown from './Dropdown.svelte';
 	import { isEditorVisible, editorInput } from './../../stores.js';
 
-	export let text;
+	export let note;
 
 	const openEditor = () => {
 		isEditorVisible.update((isEditorVisible) => true);
-		editorInput.update((editorInput) => text);
+		editorInput.update((editorInput) => note.text);
 	};
 
 	const svelteMarkdownOptions = {
@@ -25,39 +25,27 @@
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="note">
-	<div class="top">
-		<div class="title">Title</div>
+	<div class="wrap">
 		<Dropdown menuItems={dropdownItems} />
 	</div>
-	<div class="bottom">
-		<MdPreview><SvelteMarkdown source={text} options={svelteMarkdownOptions} /></MdPreview>
-	</div>
+	<MdPreview><SvelteMarkdown source={note.text} options={svelteMarkdownOptions} /></MdPreview>
 </div>
 
 <style lang="sass">
-  @use './../../vars'
+	@use './../../vars'
 
-  .note
-    border: vars.$misc-border-dark
-    border-radius: vars.$misc-borderRadius
-    color: vars.$color-text-2
-    transition: boxs-shadow 60ms linear
-    background: transparentize(vars.$color-dark-6, 0.8)
-    &:hover
-      box-shadow: vars.$misc-boxShadow
-    .top
-      width: 100%
-      border-bottom: vars.$misc-border-dark
-      display: flex
-      justify-content: space-between
-      align-items: center
-      padding: 10px vars.$size-1
-      .title
-        font-weight: bold
-        height: vars.$misc-padding-big
-        display: flex
-        align-items: center
-        justify-content: center
-    .bottom
-      padding: vars.$misc-padding-default
+	.note
+		border: vars.$misc-border-dark
+		border-radius: vars.$misc-borderRadius
+		color: vars.$color-text-2
+		transition: boxs-shadow 60ms linear
+		background: transparentize(vars.$color-dark-6, 0.8)
+		padding: vars.$misc-padding-small vars.$misc-padding-default
+		position: relative
+		&:hover
+			box-shadow: vars.$misc-boxShadow
+		.wrap
+			position: absolute
+			top: vars.$misc-padding-small
+			right: vars.$misc-padding-default
 </style>
